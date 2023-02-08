611405116c02cf35d274310fad9f61c1e91d706e
ZeckDev <122693027+Zeck47@users.noreply.github.com>
2023-02-08 01:52:34 -0300

    Create servidor-socket.py

---
+++ servidor-socket.py
@@ -0,0 +1,34 @@
+import socket 
+
+host = 'localhost'    
+port = 50000  
+
+addr = (host, port) 
+
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serv_socket.bind(addr) 
+serv_socket.listen(3) 
+
+print('aguardando conexao')
+ 
+conn, cliente = serv_socket.accept() 
+
+print('conectado em:', cliente) 
+
+
+while True:
+	data = conn.recv(1024) 
+	
+	if not data:
+		print('Fechando a conexao')
+		conn.close()
+		break
+	print("mensagem recebida:", data)
+	conn.sendall(data)
+  
+  
+  
+  
+  
+  
